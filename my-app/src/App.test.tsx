eb46d51dd3ba2344a25b4bd26948ebdbd01c811b
shihui-huang <52117621+shihui-huang@users.noreply.github.com>
2022-04-04 14:12:34 +0200

    Add integration test

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import {render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('Integration test', () => {
+  test('integration test', async () => {
+   render(<App />);
+
+    const addButton = await screen.findByText(/ajouter/i);
+
+    userEvent.click(addButton);
+    const modal = await screen.findByText(/ajouter une nouvelle destination/i);
+
+    expect(modal).toBeVisible();
+
+    const destinationInput = screen.getByPlaceholderText(/nom de la destination/i);
+    const adressInput = screen.getByPlaceholderText(/Adress/i);
+    const nbResidentInput = screen.getByPlaceholderText(/Nb Habitants/i);
+    const nbHotelInput = screen.getByPlaceholderText(/Nb Hôtel/i);
+    const nbSalaryMoyInput = screen.getByPlaceholderText(/Revenu Moy/i);
+    const nbSurfaceInput = screen.getByPlaceholderText(/Superficie/i);
+
+    const expectedDestination = "Paris";
+    userEvent.type(destinationInput, expectedDestination);
+    userEvent.type(adressInput, 'Pl. de l\'Hôtel de Ville, 75004 Paris');
+    userEvent.type(nbResidentInput, '2.1');
+    userEvent.type(nbHotelInput, '7500');
+    userEvent.type(nbSalaryMoyInput, '50000');
+    userEvent.type(nbSurfaceInput, '105.4');
+
+
+    userEvent.click(screen.getByText(/CONFIRM/i));
+    expect(await screen.findByText(expectedDestination)).toBeVisible();
+    expect(screen.getAllByRole('img', {name: 'destination'}).length).toBe(1);
+
+  })
+})
--- my-app/src/setupTests.ts
+++ my-app/src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mocking `window.matchMedia()` method which are not implemented in JSDOM
+
+global.matchMedia = global.matchMedia || function () {
+  return {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+  };
+};
